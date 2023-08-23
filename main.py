df18a39e48f5fc897eb208bc918ada4da4e7d2ef
Patryk <Patrykp295@gmail.com>
2023-08-23 03:16:21 +0200

    Encryption

---
+++ main.py
@@ -0,0 +1,19 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+def encrypt(text, shift):
+    encrypted_word = ""
+    for letter in text:
+        letter_index = alphabet.index(letter)
+        shifted_letter = alphabet[letter_index + shift]
+        encrypted_word += shifted_letter
+    print(encrypted_word)
+
+
+encrypt(text=text, shift=shift)
